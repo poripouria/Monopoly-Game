a1745e6eeb0825b63f82cec18b1948338b6b4c07
Poripouria <poripouria9@gmail.com>
2023-03-28 14:40:35 +0430

    add some properties and details

--- monopoly_classes.py
+++ monopoly_classes.py
@@ -1,8 +1,11 @@
 import random
+import numpy as np
+import pandas as  pd
 
 class Player:
-    def __init__(self, name, money=1500):
+    def __init__(self, name, appearance, money=1500):
         self.name = name
+        self.appearance = appearance
         self.money = money
         self.properties = []
         self.position = 0
@@ -12,3 +15,31 @@ class Player:
         self.doubles = 0
         self.doubles_rolls = 0
         
+        def move(self, steps):
+            self.position = (self.position + steps) % 40
+
+        def buy_property(self, property):
+            self.properties.append(property)
+            self.money -= property.price
+
+        def pay_rent(self, property):
+            rent = property.rent
+            if property.owner and property.owner != self:
+                self.money -= rent
+                property.owner.money += rent
+
+        def is_bankrupt(self):
+            return self.money < 0
+
+class Property: #Cities and places
+    def __init__(self, name, price, rent):
+        self.name = name
+        self.price = price
+        self.rent = rent
+        self.owner = None
+class Chance:
+    def __init__(self, name, action):
+        self.name = name
+        self.action = action   
+
+
