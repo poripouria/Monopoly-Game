476df272e6c654ed7bd45d738b17154b37d3faec
Poripouria <poripouria9@gmail.com>
2023-03-29 14:29:14 +0430

    Expand Game Abilities

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import random
 import numpy as np
 import pandas as  pd
 from monopoly_classes import *
+from monopoly_AI_agent import *
 
 """
 def AI_Agent(player):
@@ -40,56 +41,23 @@ def AI_Agent(player):
                 else:
                     player.buy_property(property_place[player.position])
 """
-
-dices = DoubleDice()
-
-def play_monopoly(players_num=2, max_rounds=60, AI_Agent_Mode=False):
-        # ----------------  defining properties  ---------------- #
-    property_place = []
-    df = pd.read_excel("_Data/Properties-Detail.xlsx")
-    for place in df["place"]:
-        property_place.append(place)
-    property_type = []
-    df = pd.read_excel("_Data/Properties-Detail.xlsx")
-    for type in df["type"]:
-        property_type.append(type)
-    property_country = []
-    df = pd.read_excel("_Data/Properties-Detail.xlsx")
-    for country in df["country"]:
-        property_country.append(country)
-    property_price = []
-    df = pd.read_excel("_Data/Properties-Detail.xlsx")
-    for price in df["price"]:
-        property_price.append(price)
-    property_rent = []
-    df = pd.read_excel("_Data/Properties-Detail.xlsx")
-    for rent in df["rent"]:
-        property_rent.append(rent)
-
-    properties = []
-    for i in range(40):
-        properties.append(Property(property_place[i], 
-                                    property_type[i], 
-                                    property_country[i], 
-                                    property_price[i], 
-                                    property_rent[i]))
-
-        # ----------------  defining players  ---------------- #
-    players = []
-    for i in range(players_num):
-        name = input(f"Enter the name of player {i+1} : ")
-        if name == "AI" or name == "AI Agent" or name == "Agent":
-            AI_Agent_Mode = True            
-        players.append(Player(name))
-
-        # ----------------  start game  ---------------- #
-    while input("Inter \"c\" to continue or \"end\" to end this game: ") != "end":
+def play_monopoly(players_num=2, AI_Agent_Mode=False, max_rounds=60):
+        # ----------------      start game    ---------------- #
+    round = 0
+    while input("Inter \"c\" to continue or \"end\" to end this game: ") != "end" and round < max_rounds:
         for i in range(players_num):
             if players[i].is_bankrupt():
+                print(f"{players[i].name} is bankrupt")
+                players_num -= 1
+                if players_num == 1:
+                    print(f"{players[i].name} is the winner")
+                    break
                 continue
+            # TODO_: Complete AI Agent Mode
             if AI_Agent_Mode:
                 print(f"AI Agent is playing for {players[i].name}")
                 AI_Agent(players[i])
+                pass
             else:
                 print(f"{players[i].name}'s turn")
                 d1, d2, roll_result = dices.roll_double_dice()
@@ -99,6 +67,7 @@ def play_monopoly(players_num=2, max_rounds=60, AI_Agent_Mode=False):
                         players[i].doubles_rolls = 0
                         print(f"{players[i].name} went to jail becouse of 3 doubles rolls.")
                         continue
+                    players[i].doubles = True
                     players[i].doubles_rolls += 1
                 print(f"{players[i].name} rolled {d1} and {d2}")
                 players[i].move(roll_result)
@@ -142,30 +111,70 @@ def play_monopoly(players_num=2, max_rounds=60, AI_Agent_Mode=False):
                         rand_mony = random.randint(1, 10)*10
                         print(f"{players[i].name} got ${rand_mony} from the bank!")
                         players[i].money += rand_mony
-                if players[i].jail_turns > 0:
-                    players[i].jail_turns -= 1
-                    players[i].jail = False
 
                 print("GAME STATE till now: ")
                 print(f"| {players[i].name} has {players[i].doubles_rolls} doubles rolls")
+                print(f"| {players[i].name} {'doubles' if players[i].jail else 'not doubles'} roll ({d1}, {d2})")
                 print(f"| {players[i].name} has ${players[i].money} money left")
                 print(f"| {players[i].name} has {players[i].properties} properties")
                 print(f"| {players[i].name} has ${players[i].properties_value} properties value")
                 print(f"| {players[i].name} has {players[i].countries} countries")
                 print(f"| {players[i].name} is on {players[i].position} position")
-                print(f"| {players[i].name} is {'in' if players[i].jail else 'not in'} jail")
                 print(f"| {players[i].name} has {players[i].jail_cards} jail cards")
+                print(f"| {players[i].name} is {'in' if players[i].jail else 'not in'} jail")
                 print(f"| {players[i].name} has {players[i].jail_turns} jail turns")
                 print(f"| {players[i].name} is {'bankrupt' if players[i].is_bankrupt() else 'not bankrupt'}")
-                # print(players, "\n", properties, "\n")
 
-            # TODO_: Correct this function to check if the game is over or not
-            if players[i].is_bankrupt():
-                print(f"{players[i].name} is bankrupt")
-                players_num -= 1
-                if players_num == 1:
-                    print(f"{players[i].name} is the winner")
-                    break
+                if players[i].jail_turns > 0:
+                    players[i].jail_turns -= 1
+                    players[i].jail = False
+                if players[i].doubles == True:
+                    print(f"{players[i].name} rolled double ({d1}, {d2}), his/her turn again!")
+                    players[i].doubles = False
+            
+        print(f"\n--------------- ROUND {round+1} / {max_rounds} END ---------------\n")
+        round += 1      
+
+dices = DoubleDice()
+        # ----------------  defining properties  ---------------- #
+df = pd.read_excel("_Data/Properties-Detail.xlsx")
+property_place = []
+for place_ in df["place"]:
+    property_place.append(place_)
+property_type = []
+for type_ in df["type"]:
+    property_type.append(type_)
+property_country = []
+for country_ in df["country"]:
+    property_country.append(country_)
+property_price = []
+for price_ in df["price"]:
+    property_price.append(price_)
+property_rent = []
+for rent_ in df["rent"]:
+    property_rent.append(rent_)
+
+properties = []
+for i in range(40):
+    properties.append(Property(property_place[i], 
+                                property_type[i], 
+                                property_country[i], 
+                                property_price[i], 
+                                property_rent[i]))
 
 if __name__ == "__main__":
-    play_monopoly()
+        # ----------------  defining players  ---------------- #
+    players_num = int(input(f"Enter the number of players (2 or 4): "))
+    if players_num == 2 or players_num == 4:
+        players = []
+        AI_Agent_Mode = False
+        for i in range(players_num):
+            name = input(f"Enter the name of player {i+1} : ")
+            if name == "AI" or name == "AI Agent" or name == "Agent":
+                AI_Agent_Mode = True            
+            players.append(Player(name))
+    else:
+        print("Number of players must be 2 or 4!")
+        pass
+        # ----------------       play       ---------------- #
+    play_monopoly(players_num)
