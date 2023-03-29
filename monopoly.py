69fc47105d8cde43f0fd206f527ba1680721c02c
Poripouria <poripouria9@gmail.com>
2023-03-29 23:55:03 +0430

    improve modularity

--- monopoly.py
+++ monopoly.py
@@ -13,4 +13,120 @@ class DoubleDice():
         self.rolled_sum = self.current_rolled1 + self.current_rolled2
         return self.current_rolled1, self.current_rolled2, self.rolled_sum
 
+def display_game_state(players, properties):
+    for p in players:
+        p.print_player_status()
+    for p in properties:
+        p.print_property_status()
 
+def monopoly_game(players, properties, players_num=2, AI_Agent_Mode=False, max_rounds=60):
+        # ----------------      start game    ---------------- #
+    round = 0
+    while round < max_rounds and input("Inter \"c\" to continue or \"end\" to end this game: ") != "end":
+        # TODO_: Complete AI Agent Mode
+        if AI_Agent_Mode:
+            for i, p in enumerate(players):
+                print(f"Player {i+1}: Name: {p.name}, is {type(p).__name__}")
+
+        for turn_counter in range(players_num):
+            current_player = players[turn_counter]
+            if current_player.is_bankrupt():
+                print(f"{current_player.name} is bankrupt")
+                players_num -= 1
+                for p in players:
+                    if p != current_player:
+                        p.money += current_player.money / (players_num - 1)
+                players.remove(current_player)
+                if players_num == 1:
+                    print(f"{current_player.name} is the WINNER")
+                    return
+            if current_player.jail:
+                break
+            play_monopoly(current_player, players, properties)
+            if current_player.doubles:
+                play_monopoly(current_player, players, properties)
+                if current_player.doubles:
+                    play_monopoly(current_player, players, properties)
+                    if current_player.doubles:
+                        play_monopoly(current_player, players, properties)
+
+        print(f"\n--------------- ROUND {round+1} / {max_rounds} END ---------------\n")
+        round += 1
+
+def play_monopoly(current_player, players, properties):
+    dices = DoubleDice()
+    print(f"{current_player.name}'s turn")
+    d1, d2, roll_result = dices.roll_double_dice()
+    if d1 == d2:
+        if current_player.doubles_rolls > 2:
+            current_player.jail = True
+            current_player.doubles_rolls = 0
+            print(f"{current_player.name} went to jail becouse of 3 doubles rolls.")
+            pass
+        if current_player.jail:
+            current_player.jail_turns -= 1
+            print(f"{current_player.name} is still in jail and couldn't roll again.")
+            pass
+        current_player.doubles = True
+        current_player.doubles_rolls += 1
+    else:
+        current_player.doubles = False
+        current_player.doubles_rolls = 0
+
+    print(f"{current_player.name} rolled {d1} and {d2}")
+    current_player.move(roll_result)
+    print(f"{current_player.name} is on {properties[current_player.position].name}")
+    
+    if properties[current_player.position].type == "city" or properties[current_player.position].type == "service_centers":
+        if properties[current_player.position].owner != None and properties[current_player.position].owner != current_player:
+            print(f"{current_player.name} has to pay ${properties[current_player.position].rent} to {properties[current_player.position].owner.name}")
+            current_player.pay_rent(properties[current_player.position])
+        else:
+            print(f"{current_player.name} can buy {properties[current_player.position].name} for ${properties[current_player.position].price}")
+            if input(f"Do you want to buy it (you have ${current_player.money})? (y/n) ") == "y":
+                if current_player.money < properties[current_player.position].price:
+                    print("You don't have enough money to buy it") 
+                else:
+                    print(f"You bought {properties[current_player.position].name}.")
+                    current_player.buy_property(properties[current_player.position])
+            else:
+                print(f"You didn't buy {properties[current_player.position].name}.")
+                pass
+    if properties[current_player.position].type == "stay_place":
+        if properties[current_player.position].name == "Go (Collect $200)":
+            pass
+        elif properties[current_player.position].name == "Jail":
+            if current_player.jail_cards > 0 and input(f"Do you want to use your Jail-Free card? (y/n) ") == "y":
+                current_player.jail_cards -= 1
+                current_player.jail = False
+                print(f"{current_player.name} used a get out of jail free card.")
+            current_player.position = 10
+            current_player.jail = True
+            current_player.jail_turns += 1
+            print(f"{current_player.name} went to jail.")
+        elif properties[current_player.position].name == "Auction (Trade)":
+            print("Currently Auction (Trade) is not available!")
+            pass
+        elif properties[current_player.position].name == "Free Parking":
+            pass
+        elif properties[current_player.position].name == "Chance":
+            current_player.chance(players)
+        elif properties[current_player.position].name == "Income Tax":
+            print(f"{current_player.name} paied ${0.1 * current_player.money} to the bank for Income Tax!")
+            current_player.money -= 0.1 * current_player.money
+        elif properties[current_player.position].name == "Luxury Tax":
+            current_player.money -= 200
+            print(f"{current_player.name} paied $200 to the bank for Luxury Tax!")
+        elif properties[current_player.position].name == "Treasure":
+            rand_mony = random.randint(1, 10)*10
+            print(f"{current_player.name} got ${rand_mony} from the bank!")
+            current_player.money += rand_mony
+
+    print(" _________________________ GAME PLAYERS STATUS TILL NOW: _________________________ ")
+    display_game_state(players, properties)
+
+    if current_player.jail_turns > 0:
+        current_player.jail_turns -= 1
+        current_player.jail = False
+    if current_player.doubles:
+        print(f"{current_player.name} rolled double ({d1}, {d2}), his/her turn again!")
--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 class Player:
     def __init__(self, name, appearance=None, money=1500):
@@ -114,3 +115,215 @@ class Player:
 
     def __repr__(self):
         return (str(self.name))
+
+class AI_Agent(Player):
+    def __init__(self, name, appearance=None, money=1500, depth=5):
+        super().__init__(name, appearance, money)
+        self.depth = depth
+
+    def expectimax(self, state, depth):
+        if depth == 0:
+            # evaluate the state statically
+            return self.evaluate(state)
+
+        if state.is_terminal:
+            return state.utility(self)
+
+        if state.current_player == self:
+            # maximize by picking the best action
+            value = float('-inf')
+            for action in state.actions():
+                child_state = state.result(action)
+                v = self.expectimax(child_state, depth)
+                value = max(value, v)
+            return value
+        else:
+            # expected value by averaging over all possible actions
+            value = 0
+            count = 0
+            for action in state.actions():
+                child_state = state.result(action)
+                v = self.expectimax(child_state, depth - 1)
+                value += v
+                count += 1
+            return value / count
+
+    def evaluate(self, state):
+        # basic static evaluation function that only considers the amount of money the player has
+        return self.money
+
+    def play(self, state):
+        best_value = float('-inf')
+        best_action = None
+        for action in state.actions():
+            child_state = state.result(action)
+            value = self.expectimax(child_state, self.depth)
+            if value > best_value:
+                best_value = value
+                best_action = action
+        return best_action
+
+class AI_Agent_MiniMax(Player):
+    def __init__(self, name, appearance=None, money=1500, depth=5):
+        super().__init__(name, appearance, money)
+        self.depth = depth
+
+    def minimax(self, state, depth, is_maximizing):
+        if depth == 0 or state.is_game_over():
+            return state.evaluate_state(), None
+
+        if is_maximizing:
+            best_value = -np.inf
+            best_move = None
+            for move in state.get_possible_moves(self):
+                new_state = state.get_new_state(move, self)
+                move_value, _ = self.minimax(new_state, depth - 1, False)
+                if move_value > best_value:
+                    best_value = move_value
+                    best_move = move
+            return best_value, best_move
+        else:
+            best_value = np.inf
+            best_move = None
+            for move in state.get_possible_moves(self):
+                new_state = state.get_new_state(move, self)
+                move_value, _ = self.minimax(new_state, depth - 1, True)
+                if move_value < best_value:
+                    best_value = move_value
+                    best_move = move
+            return best_value, best_move
+
+    def make_move(self, state):
+        _, best_move = self.minimax(state, self.depth, True)
+        return best_move
+
+
+    def move(self, player, properties, players):
+        """
+        Choose the optimal action for a player in a Monopoly game 
+        using the ExpectMinMax algorithm. Here the agent plays to 
+        maximize its profits.
+        """
+        actions = self.get_legal_moves(player, properties)
+        _, action = self.expectimax_decision(player, properties, players, 0, True)
+        assert action in actions
+        self.do_action(player, properties, action, players)
+
+    def expectimax_decision(self, player, properties, players, depth, maximizing_player):
+        """
+        Returns the optimal action to take for a player using 
+        the Expectimax algorithm.
+        """
+        if player.is_bankrupt():
+            return -np.inf, None
+        
+        if depth == self.depth:
+            return self.calc_board_utility(player, properties), None
+
+        if maximizing_player:
+            v_max = -np.inf
+            best_action = None
+            for action in self.get_legal_moves(player, properties):
+                copy_player = player.copy()
+                copy_properties = self.get_copy_properties(properties)
+                copy_players = self.get_copy_players(players)
+                self.do_action(copy_player, copy_properties, action, copy_players)
+                v, _ = self.expectimax_decision(copy_player, copy_properties, copy_players, depth + 1, False)
+                if v > v_max:
+                    v_max, best_action = v, action
+            return v_max, best_action
+            
+        else:
+            v_exp = 0
+            actions = self.get_legal_moves(player, properties)
+            for action in actions:
+                copy_player = player.copy()
+                copy_properties = self.get_copy_properties(properties)
+                copy_players = self.get_copy_players(players)
+                self.do_action(copy_player, copy_properties, action, copy_players)
+                v, _ = self.expectimax_decision(copy_player, copy_properties, copy_players, depth + 1, True)
+                v_exp += v / len(actions)
+            return v_exp, None
+        
+    def get_legal_moves(self, player, properties):
+        """
+        Returns all legal moves for a player in a Monopoly game.
+        """
+        moves = []
+        dices = DoubleDice()
+        d1, d2, roll_result = dices.roll_double_dice()  
+        if player.jail and player.jail_cards > 0:
+            moves.append('use jail card')
+        elif player.jail:
+            moves.append('do nothing')
+        else:
+            moves.append('end turn')
+            if d1 == d2:
+                moves.append('roll dice again')
+                if player.doubles_rolls < 2:
+                    moves.append('move')
+            else:
+                moves.append('move')
+        return moves
+
+    def do_action(self, player, properties, action, players):
+        """
+        Performs the given action for the agent. Here agent 
+        receives utility for moving to other tiles and receives 
+        rewards for collecting rent for its owned tiles.
+        """
+        if action == 'use jail card' and player.jail_cards > 0:
+            player.jail = False
+            player.jail_cards -= 1
+        elif action == 'do nothing':
+            player.jail_turns += 1
+        elif action == 'end turn':
+            player.jail_turns = max(0, player.jail_turns - 1)
+        elif action == 'move':
+            dices = DoubleDice()
+            d1, d2, roll_result = dices.roll_double_dice()  
+            player.move(roll_result)
+            if properties[player.position].owner and properties[player.position].owner != player:
+                player.pay_rent(properties[player.position])
+            elif player.money > properties[player.position].price * 1.1:
+                player.buy_property(properties[player.position])
+        elif action == 'roll dice again':
+            player.move(d1+d2)
+            player.doubles_rolls += 1
+            if properties[player.position].owner and properties[player.position].owner != player:
+                player.pay_rent(properties[player.position])
+            elif player.money > properties[player.position].price * 1.1:
+                player.buy_property(properties[player.position])
+        else:
+            raise Exception('Invalid action')
+
+    def calc_board_utility(self, player, properties):
+        """
+        Evaluates the game utility for each player in a game. Here the agent 
+        evaluates the sum of its tile property values minus the sum of its 
+        players debts.
+        """
+        utility = 0
+        for p in properties:
+            if p.owner == player:
+                utility += p.price
+        utility -= player.get_debt()
+        return utility
+    
+    def get_copy_properties(self, properties):
+        """
+        Returns a deepcopy of a list of properties.
+        """
+        copy_properties = []
+        for p in properties:
+            copy_properties.append(p.copy())
+        return copy_properties
+
+    def get_copy_players(self, players):
+        """
+        Returns a deepcopy of a list of players.
+        """
+        copy_players = []
+        for player in players:
+            copy_players.append(player.copy())
+        return copy_players
